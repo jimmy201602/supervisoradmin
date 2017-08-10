964209a2a90afb4524fd4f6c03368fe9af9ed781
Jimmy <zhengge2012@gmail.com>
2017-08-10 18:06:13 +0800

    initalize project

---
+++ superadmin/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+class NodeEnv(models.Model):
+    name = models.CharField(_('Name'),max_length=50,help_text=_('Node name'))
+    username = models.CharField(_('Username'),max_length=50,help_text=_('Supervisor login user name'))
+    password = models.CharField(_('Password'),max_length=50,help_text=_('Supervior login password'))
+    port = models.PositiveIntegerField(_('Port'),help_text=_('Supervisor remote manage port'))
+
+    def __unicode__(self):
+        return self.name
+
+class EnvironmentGroups(models.Model):
+    name = models.CharField(_('Name'),max_length=50,help_text=_('Environment name'))
+    members = models.ManyToManyField(NodeEnv,help_text=_('Environment members'))
+
+    def __unicode__(self):
+        return  self.name
+
+class Groups(models.Model):
+    name = models.CharField(_('Name'),max_length=50,help_text=_('Group name'))
+    members = models.ManyToManyField(EnvironmentGroups,help_text=_('Group members'))
+    def __unicode__(self):
+        return self.name
+
+class Log(models.Model):
+    module_name = models.CharField(_('Module name'),max_length=50,help_text=_('Log module name'))
+    level = models.CharField(_('level'),max_length=50,help_text=_('Log level'))
+    message = models.TextField(_('Message'),max_length=2000,help_text=_('Log detail message'))
+    create_datetime = models.DateTimeField(_('Create datetime'),auto_now_add=True,help_text=_('Created datetime'))
