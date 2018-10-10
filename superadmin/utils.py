58cc7d685892bc6ff89730092d4d98a7ec230f7c
jimmy201602 <zhengge2012@gmail.com>
2018-10-10 22:15:34 +0800

    pep8 format

--- superadmin/utils.py
+++ superadmin/utils.py
@@ -2,7 +2,8 @@ import xmlrpclib
 import ConfigParser
 from datetime import datetime, timedelta
 from django.http import JsonResponse
-from superadmin.settings import activity_log,CONFIG_FILE
+from superadmin.settings import activity_log, CONFIG_FILE
+
 
 class TimeoutTransport(xmlrpclib.Transport):
 
@@ -20,10 +21,12 @@ class TimeoutServerProxy(xmlrpclib.ServerProxy):
 
     def __init__(self, uri, timeout=3, transport=None, encoding=None, verbose=0, allow_none=0, use_datetime=0):
         t = TimeoutTransport(timeout)
-        xmlrpclib.ServerProxy.__init__(self, uri, t, encoding, verbose, allow_none, use_datetime)
+        xmlrpclib.ServerProxy.__init__(
+            self, uri, t, encoding, verbose, allow_none, use_datetime)
+
 
 class Config:
-    
+
     def __init__(self, CFILE):
         self.CFILE = CFILE
         self.cfg = ConfigParser.ConfigParser()
@@ -44,14 +47,14 @@ class Config:
             if name[:5] == 'group':
                 self.group_list.append(name[6:])
 
-        
     def getNodeConfig(self, node_name):
         self.node_name = "node:%s" % (node_name)
         self.username = self.cfg.get(self.node_name, 'username')
         self.password = self.cfg.get(self.node_name, 'password')
         self.host = self.cfg.get(self.node_name, 'host')
         self.port = self.cfg.get(self.node_name, 'port')
-        self.node_config = NodeConfig(self.node_name, self.host, self.port, self.username, self.password)
+        self.node_config = NodeConfig(
+            self.node_name, self.host, self.port, self.username, self.password)
         return self.node_config
 
     def getMemberNames(self, environment_name):
@@ -63,6 +66,7 @@ class Config:
     def getActivityLog(self):
         return str(activity_log)
 
+
 class NodeConfig:
 
     def __init__(self, node_name, host, port, username, password):
@@ -71,19 +75,20 @@ class NodeConfig:
         self.port = port
         self.username = username
         self.password = password
-            
+
 
 class Node:
 
     def __init__(self, node_config):
         self.long_name = node_config.node_name
         self.name = node_config.node_name[5:]
-        self.connection = Connection(node_config.host, node_config.port, node_config.username, node_config.password).getConnection()
-        self.process_list=[]
-        self.process_dict2={}
+        self.connection = Connection(node_config.host, node_config.port,
+                                     node_config.username, node_config.password).getConnection()
+        self.process_list = []
+        self.process_dict2 = {}
         for p in self.connection.supervisor.getAllProcessInfo():
             self.process_list.append(ProcessInfo(p))
-            self.process_dict2[p['group']+':'+p['name']] = ProcessInfo(p)
+            self.process_dict2[p['group'] + ':' + p['name']] = ProcessInfo(p)
         self.process_dict = self.connection.supervisor.getAllProcessInfo()
 
 
@@ -94,11 +99,12 @@ class Connection:
         self.port = port
         self.username = username
         self.password = password
-        self.address = "http://%s:%s@%s:%s/RPC2" %(self.username, self.password, self.host, self.port)
+        self.address = "http://%s:%s@%s:%s/RPC2" % (
+            self.username, self.password, self.host, self.port)
 
     def getConnection(self):
         return TimeoutServerProxy(self.address)
-        
+
 
 class ProcessInfo:
 
@@ -107,9 +113,12 @@ class ProcessInfo:
         self.name = self.dictionary['name']
         self.group = self.dictionary['group']
         self.start = self.dictionary['start']
-        self.start_hr = datetime.fromtimestamp(self.dictionary['start']).strftime('%Y-%m-%d %H:%M:%S')[11:]
-        self.stop_hr = datetime.fromtimestamp(self.dictionary['stop']).strftime('%Y-%m-%d %H:%M:%S')[11:]
-        self.now_hr = datetime.fromtimestamp(self.dictionary['now']).strftime('%Y-%m-%d %H:%M:%S')[11:]
+        self.start_hr = datetime.fromtimestamp(
+            self.dictionary['start']).strftime('%Y-%m-%d %H:%M:%S')[11:]
+        self.stop_hr = datetime.fromtimestamp(
+            self.dictionary['stop']).strftime('%Y-%m-%d %H:%M:%S')[11:]
+        self.now_hr = datetime.fromtimestamp(
+            self.dictionary['now']).strftime('%Y-%m-%d %H:%M:%S')[11:]
         self.stop = self.dictionary['stop']
         self.now = self.dictionary['now']
         self.state = self.dictionary['state']
@@ -122,8 +131,9 @@ class ProcessInfo:
         self.seconds = self.now - self.start
         self.uptime = str(timedelta(seconds=self.seconds))
 
+
 class JsonValue:
-    
+
     def __init__(self, process_name, node_name, event):
         self.process_name = process_name
         self.event = event
@@ -132,19 +142,19 @@ class JsonValue:
         self.node = Node(self.node_config)
 
     def success(self):
-        return JsonResponse({'status' : "Success",
-                       'code' : 80,
-                       'message' : "%s %s %s event succesfully" %(self.node_name, self.process_name, self.event),
-                       'nodename' : self.node_name,
-                       'data' : self.node.connection.supervisor.getProcessInfo(self.process_name)})
+        return JsonResponse({'status': "Success",
+                             'code': 80,
+                             'message': "%s %s %s event succesfully" % (self.node_name, self.process_name, self.event),
+                             'nodename': self.node_name,
+                             'data': self.node.connection.supervisor.getProcessInfo(self.process_name)})
 
-    def error(self, code, payload):     
+    def error(self, code, payload):
         self.code = code
         self.payload = payload
-        return JsonResponse({'status' : "Error",
-                       'code' : self.code,
-                       'message' : "%s %s %s event unsuccesful" %(self.node_name, self.process_name, self.event),
-                       'nodename' : self.node_name,
-                       'payload' : self.payload})
- 
+        return JsonResponse({'status': "Error",
+                             'code': self.code,
+                             'message': "%s %s %s event unsuccesful" % (self.node_name, self.process_name, self.event),
+                             'nodename': self.node_name,
+                             'payload': self.payload})
+
 
